6de2fbeb9370f316e4d100eb63d37874b4df0f14
Ubuntu <ubuntu@ip-172-31-40-137.us-west-2.compute.internal>
2013-07-12 06:08:19 +0000

    add toString

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ var data = fs.readFileSync('index.html')
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(data);
+  response.send(data.toString());
 });
 
 var port = process.env.PORT || 5000;
